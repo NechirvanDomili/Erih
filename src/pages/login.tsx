b474ec17df69fb2c0c70b884dccc7edd7703e9dd
NechirvanDomili <125748949+NechirvanDomili@users.noreply.github.com>
2025-06-18 13:45:24 +0200

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import { useState, type FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import './Login.css';
+import './login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
